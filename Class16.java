80cef4b0d4c0359175cf5ba5c77b0129fec76d17
Skes98055 <91359104+Skes98055@users.noreply.github.com>
2022-11-30 00:38:51 +0800

    Create Class16.java

---
+++ Class16.java
@@ -0,0 +1,10 @@
+import  java.util.*;
+public class Class16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a="262904713";
+        int b=Integer.parseInt(a);
+        int c=b*2;
+        System.out.println(c);
+    }
+}
