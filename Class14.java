9ba743a35e456bb03524f2f16528cce5cc524877
Skes98055 <91359104+Skes98055@users.noreply.github.com>
2022-11-30 00:49:10 +0800

    Create Class14.java

---
+++ Class14.java
@@ -0,0 +1,7 @@
+import  java.util.*;
+public class Class14 {
+    public static void main(String[] args) {
+        String a="Habit is a second nature";
+        System.out.println(a.substring(18,24));
+    }
+}
